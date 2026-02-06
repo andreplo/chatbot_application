106a36c4f467543a400e7e8532e304c28bb5efe7
andreplo@live.com <andreplo@live.com>
2026-02-06 22:24:52 +0100

    tailwind css configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>This is my Chatbot Application</h1>;
+  return (
+    <h1 className="px-4 py-2 rounded bg-blue-600 text-white hover:bg-blue-700">
+      This is my Chatbot Application
+    </h1>
+  );
 }
 
 export default App;
